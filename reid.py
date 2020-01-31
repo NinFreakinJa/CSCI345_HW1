81d1d3d0f90724fc4f04ab0289a93bd695b01f48
Reid Foster <j.reid.foster@gmail.com>
2020-01-31 15:37:41 -0500

    Update reid.py

--- reid.py
+++ reid.py
@@ -29,25 +29,38 @@ def appendToFile(cracked_hashes, cracked_passwords):
 
 # Cracks a seven-character word whose first character is capitalized and a
 # single-digit number is appended.
-def sevenCharWithDigit(password_hashes):
-    cracked_hashes = []
-    cracked_passwords = []
-
-    # Method body.
-
-    appendToFile(cracked_hashes, cracked_passwords)
+# Called within main dictionary cracking method
+def sevenCharWithDigit(password_hashes,testValue):
+    temp=testValue.capitalize()
+    #Tested with each number
+    for i in range(10):
+        #returns none if nothing to test
+        if not password_hashes:
+            return None
+        temp2=temp+str(i)
+        hashTest=hashlib.sha256(temp2).hexdigest()
+        # Test each password
+        cracked_hashes=[j for j in password_hashes if (hashTest==j)]
+        cracked_passwords=[]
+        password_hashes=[k for k in password_hashes if j not in cracked_hashes]
+        for k in range(len(cracked_hashes)):
+            cracked_passwords.append(temp2)
+        appendToFile(cracked_hashes,cracked_passwords)
+        #for j in password_hashes:
+        #    if (hashTest==j):
+         #           appendToFile([hashTest],[temp2])
+          #          password_hashes.remove(j)
     return password_hashes
 
 # Cracks a single word.
 def singleWord(password_hashes):
     cracked_hashes = []
     cracked_passwords = []
-    # Keep track of five and seven-char words to provide as input to
-    # other methods.
-    five_char_words = []
-    seven_char_words = []
-
-    # Method body.
+    with open(WORD_DIR, "r") as file:
+        lines = file.readlines()
+    for i in range(0, len(lines)):
+        if not password_hashes:
+            return None
 
     appendToFile(cracked_hashes, cracked_passwords)
     # Running other word-based attacks from here using only words that
@@ -73,16 +86,22 @@ def __digitHelper(password_hashes, currTest):
         #if the string is not empty
         if currTest:
             #create and compare hashes
-            hashTest=hashlib.sha256(currTest).hexdigest
-            for i in password_hashes:
-                if (hashTest==i):
-                    appendToFile([hashTest],[currTest])
-                    password_hashes.remove(i)
+            hashTest=hashlib.sha256(currTest).hexdigest()
+            cracked_hashes=[j for j in password_hashes if (hashTest==j)]
+            cracked_passwords=[]
+            password_hashes=[k for k in password_hashes if k not in cracked_hashes]
+            for k in range(len(cracked_hashes)):
+                cracked_passwords.append(currTest)
+            appendToFile(cracked_hashes,cracked_passwords)
+            #for i in password_hashes:
+             #   if (hashTest==i):
+              #      appendToFile([hashTest],[currTest])
+               #     password_hashes.remove(i)
         #if the function is 4 digits long it will check the five digit with special requirement
-        if(currTest.length==4):
-            fiveDigitWithSpecial(password_hashes,currTest)
+        if(len(currTest)==4):
+            password_hashes=fiveDigitWithSpecial(password_hashes,currTest)
         #recursively creates all possible digit combinations up to 7 digits
-        if(currTest.length<7):
+        if(len(currTest)<7):
             for i in range(10):
                 password_hashes=__digitHelper(password_hashes,currTest+str(i))
         #returns updated list of unsolved password hashes
